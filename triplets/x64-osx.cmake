873ff095c87880afdf92a34e29b800b46a9316cb
Derek Chow <derek@sbgsportssoftware.com>
2018-11-23 12:06:01 +0000

    osx triplet: target osx 10.13 as min supported version

--- triplets/x64-osx.cmake
+++ triplets/x64-osx.cmake
@@ -5,3 +5,9 @@ set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
 
+set(VCPKG_CXX_FLAGS --stdlib=libc++)
+# CMake Error at scripts/cmake/vcpkg_configure_cmake.cmake:160 (message):
+#  You must set both the VCPKG_CXX_FLAGS and VCPKG_C_FLAGS
+set(VCPKG_C_FLAGS "")
+set(VCPKG_OSX_DEPLOYMENT_TARGET 10.13)
+
