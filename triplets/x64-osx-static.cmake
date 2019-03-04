5c266aff430ab4399893ad1e4d1a21b734330593
dereksbg <derek@sbgsportssoftware.com>
2019-03-04 20:57:52 +0000

    triplets/x64-osx-static

---
+++ triplets/x64-osx-static.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_CXX_FLAGS --stdlib=libc++)
+# CMake Error at scripts/cmake/vcpkg_configure_cmake.cmake:160 (message):
+#  You must set both the VCPKG_CXX_FLAGS and VCPKG_C_FLAGS
+set(VCPKG_C_FLAGS "")
+set(VCPKG_OSX_DEPLOYMENT_TARGET 10.8)
